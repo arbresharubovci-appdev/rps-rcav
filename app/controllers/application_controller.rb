b924ab07134d59fc6cbda985c0e7fc06a20224cc
Arbreshë Rubovci <arbresharubovci@gmail.com>
2023-02-22 22:34:14 +0000

    Lots of progress on paper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,32 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
-  def play_rock
+
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end
+  
+  
+  
+    def play_rock
      # write a ton of code
      #redirect_to("https://www.wikipedia.org")
      #render({ :html => "<h1>Howdy, world</h1>".html_safe})
-    render({ :template => "game_templates/user_rock.html.erb"})
+    
+     render({ :template => "game_templates/user_rock.html.erb"})
+   end
 
+   def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+       @outcome = "won"
+    elsif @comp_move == "paper"
+        @outcome = "tied"
+    elsif @comp_move == "scissors"
+        @outcome = "lost"
+    end
+    
+    render({ :template => "game_templates/user_paper.html.erb"})
   end
-
 end
