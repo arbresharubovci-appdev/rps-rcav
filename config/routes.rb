2dfed173c2a811a8502b5af57bb431f1b03d1d84
Arbreshë Rubovci <arbresharubovci@gmail.com>
2023-02-22 17:31:06 +0000

    Howdy, world!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Rails.application.routes.draw 
- get("/rock", { :controller => "application", :action => "play_rock"})
+Rails.application.routes.draw do
+  get("/rock", { :controller => "application", :action => "play_rock"})
 
-
+  
 end
